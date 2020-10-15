4a13bc5b0ed9fcda1f93651a8f3c0f8d6a575dfa
mekk1t <kit.kalimov@gmail.com>
2020-10-15 22:30:51 +0300

    Набросок шифратора/дешифратора

---
+++ Mekk1tCryptography/CustomCodec/MVVM/Models/EncodingDecoding/Codec.cs
@@ -0,0 +1,32 @@
+﻿using CustomCodec.MVVM.Models.EncodingDecoding.Interfaces;
+using System;
+
+namespace CustomCodec.MVVM.Models.EncodingDecoding
+{
+    public class Codec : ICodec
+    {
+        private string message;
+
+        public Codec(string message)
+        {
+            this.message = message;
+        }
+
+        public void Decode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Encode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetMessage()
+        {
+            var temp = message;
+            message = default;
+            return temp;
+        }
+    }
+}
---
+++ Mekk1tCryptography/CustomCodec/MVVM/Models/EncodingDecoding/Interfaces/ICodec.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomCodec.MVVM.Models.EncodingDecoding.Interfaces
+{
+    public interface ICodec
+    {
+        public void Encode();
+        public void Decode();
+        public string GetMessage();
+    }
+}
