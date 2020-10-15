78c7f3236287029040c4b848aee65b2272cfc5a1
mekk1t <kit.kalimov@gmail.com>
2020-10-15 22:31:04 +0300

    Набросок работы с файлами

---
+++ Mekk1tCryptography/CustomCodec/MVVM/Models/FilesManagement/FileManager.cs
@@ -0,0 +1,32 @@
+﻿using CustomCodec.MVVM.Models;
+using CustomCodec.MVVM.Models.FilesManagement.Interfaces;
+using Microsoft.Win32;
+
+namespace CustomCodec.Operations.FilesManagement
+{
+    public class FileManager : IFileManager
+    {
+        private TextFile inputFile;
+        private TextFile outputFile;
+
+        public void ReadFromFile(string filePath)
+        {
+        }
+
+        public void SaveToFile(string filePath)
+        {
+        }
+
+        public void SelectFile()
+        {
+            var ofd = new OpenFileDialog();
+
+            inputFile = new TextFile();
+        }
+
+        public void UnselectFile()
+        {
+            inputFile = null;
+        }
+    }
+}
---
+++ Mekk1tCryptography/CustomCodec/MVVM/Models/TextFile.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomCodec.MVVM.Models
+{
+    public class TextFile
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public byte[] BinaryData { get; set; }
+        public string Text { get; set; }
+    }
+}
