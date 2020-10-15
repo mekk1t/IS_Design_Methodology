14ed9c7f777efd6bde18195dc71dd7aeb1af4ff6
mekk1t <kit.kalimov@gmail.com>
2020-10-15 21:55:12 +0300

    Создан первый проект: WPF-приложение CustomCodec

---
+++ Mekk1tCryptography/CustomCodec/MVVM/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CustomCodec
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
