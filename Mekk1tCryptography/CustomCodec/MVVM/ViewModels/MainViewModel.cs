ec43b99fe47a1eb94913c51dc8980e5e43c99de5
mekk1t <kit.kalimov@gmail.com>
2020-10-15 23:07:10 +0300

    Первый драфт интерфейса

---
+++ Mekk1tCryptography/CustomCodec/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+
+namespace CustomCodec.MVVM.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private const string urlToImage = @"D:\Repositories\IS_Design_Methodology\Mekk1tCryptography\CustomCodec\icon.ico";
+        public BitmapFrame Icon { get; } = BitmapFrame.Create(new Uri(urlToImage, UriKind.Absolute));
+        public string Title { get; } = "kit.kalimov © 2020 All Rights Reserved";
+        public bool DidUserInputText { get; set; } = false;
+
+        public MainViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
