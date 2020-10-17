fa27b37c4a2cc5350743f3e8506d90000cd39cf5
mekk1t <kit.kalimov@gmail.com>
2020-10-17 10:49:43 +0300

    Финальный эскиз модели главного представления

--- Mekk1tCryptography/CustomCodec/MVVM/ViewModels/MainViewModel.cs
+++ Mekk1tCryptography/CustomCodec/MVVM/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CustomCodec.MVVM.Models.EncodingDecoding.Interfaces;
+using CustomCodec.MVVM.Models.FilesManagement.Interfaces;
+using System;
 using System.ComponentModel;
 using System.Windows.Media.Imaging;
 
@@ -8,8 +10,31 @@ namespace CustomCodec.MVVM.ViewModels
     {
         private const string urlToImage = @"D:\Repositories\IS_Design_Methodology\Mekk1tCryptography\CustomCodec\icon.ico";
         public BitmapFrame Icon { get; } = BitmapFrame.Create(new Uri(urlToImage, UriKind.Absolute));
-        public string Title { get; } = "kit.kalimov © 2020 All Rights Reserved";
+
         public bool DidUserInputText { get; set; } = false;
+        public bool IsEncoded { get; set; } = false;
+        public bool NotEncoded
+        {
+            get
+            {
+                return !IsEncoded;
+            }
+            set
+            {
+                NotEncoded = value;
+            }
+        }
+
+        public string WelcomeText { get; } =
+            "Чтобы зашифровать сообщение, введите текст в поле слева вручную или считав из файла." +
+            "Нажмите на кнопку \"Зашифровать\", чтобы получить результат.";
+
+        public string Title { get; } = "kit.kalimov © 2020 All Rights Reserved";
+        public string Input { get; set; }
+        public string Output { get; set; }
+
+        private readonly IFileManager fileManager;
+        private readonly ICodec codec;
 
         public MainViewModel()
         {
