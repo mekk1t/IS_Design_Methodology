ebf474f61992c625db5ab0e3b04cb24b0daa359b
mekk1t <kit.kalimov@gmail.com>
2020-10-18 01:04:32 +0300

    Модель объекта для хранения данных для шифрования / дешифрования

---
+++ Mekk1tCryptography/CustomCodec/MVVM/Models/AlgorithmParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomCodec_WPF.MVVM.Models
+{
+    public class AlgorithmParameters
+    {
+        public byte Mod { get; set; }
+        public string Key { get; set; }
+        public bool UseEnglish { get; set; }
+        public bool UseRussian { get; set; }
+        public string Message { get; set; }
+    }
+}
