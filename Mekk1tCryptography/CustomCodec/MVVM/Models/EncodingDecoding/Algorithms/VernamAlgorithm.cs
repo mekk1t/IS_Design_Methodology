a8e957f3a7fc03943eae08723dcbcf9f0c111f71
mekk1t <kit.kalimov@gmail.com>
2020-10-18 01:03:57 +0300

    Реализация алгоритма Виженера

--- Mekk1tCryptography/CustomCodec/MVVM/Models/EncodingDecoding/Algorithms/VernamAlgorithm.cs
+++ Mekk1tCryptography/CustomCodec/MVVM/Models/EncodingDecoding/Algorithms/VernamAlgorithm.cs
@@ -1,34 +1,149 @@
 ﻿using CustomCodec.MVVM.Models.EncodingDecoding.Interfaces;
+using CustomCodec_WPF.MVVM.Models;
 using CustomCodec_WPF.MVVM.Models.EncodingDecoding.Alphabets;
-using System;
+using System.Linq;
+using System.Text;
 
 namespace CustomCodec.MVVM.Models.EncodingDecoding
 {
     public class VernamAlgorithm : ICodec
     {
-        private string message;
+        private readonly AlgorithmParameters parameters;
         private readonly IAlphabet alphabet;
 
-        public VernamAlgorithm(string message)
+        private string keyMask;
+        private string encodedMessage;
+        private string decodedMessage;
+
+        private byte[] encodedMessageNumbered;
+        private byte[] messageNumbered;
+        private byte[] keyMaskNumbered;
+
+        private byte[] subtractByModule;
+        private byte[] sumByModule;
+
+        public VernamAlgorithm(AlgorithmParameters parameters)
         {
-            this.message = message;
+            this.parameters = parameters;
+
+            if (parameters.UseEnglish)
+                alphabet = new EnglishAlphabet();
+            else
+                alphabet = new RussianAlphabet();
         }
 
         public void Decode()
         {
-            throw new NotImplementedException();
+            ImposeKeyOnTheMessage();
+            NumberTheEncodedMessage();
+            NumberTheKeyMask();
+            SubtractByModule();
+            Decrypt();
         }
-
         public void Encode()
         {
-            throw new NotImplementedException();
+            ImposeKeyOnTheMessage();
+            NumberTheMessage();
+            NumberTheKeyMask();
+            SumByModule();
+            Encrypt();
         }
 
-        public string GetMessage()
+        public string GetEncodedMessage()
         {
-            var temp = message;
-            message = default;
-            return temp;
+            return encodedMessage;
+        }
+        public string GetDecodedMessage()
+        {
+            return decodedMessage;
+        }
+
+        private void ImposeKeyOnTheMessage()
+        {
+            var sb = new StringBuilder();
+
+            var keyLength = parameters.Key.Length;
+            var keyEntriesInMessageTimes = parameters.Message.Length / keyLength;
+            var remainingKeyLetters = parameters.Message.Length % keyLength;
+
+            for (int i = 0; i < keyEntriesInMessageTimes; i++)
+                sb.Append(parameters.Key);
+
+            if (remainingKeyLetters != 0)
+                for (byte i = 0; i < remainingKeyLetters; i++)
+                    sb.Append(parameters.Key[i]);
+
+            keyMask = sb.ToString();
+        }
+
+        private void NumberTheEncodedMessage()
+        {
+            encodedMessageNumbered = new byte[parameters.Message.Length];
+
+            for (int i = 0; i < parameters.Message.Length; i++)
+            {
+                encodedMessageNumbered[i] = alphabet.Alphabet[parameters.Message[i]];
+            }
+        }
+        private void NumberTheMessage()
+        {
+            messageNumbered = new byte[parameters.Message.Length];
+
+            for (int i = 0; i < parameters.Message.Length; i++)
+            {
+                messageNumbered[i] = alphabet.Alphabet[parameters.Message[i]];
+            }
+        }
+        private void NumberTheKeyMask()
+        {
+            keyMaskNumbered = new byte[parameters.Key.Length];
+
+            for (int i = 0; i < parameters.Key.Length; i++)
+            {
+                keyMaskNumbered[i] = alphabet.Alphabet[parameters.Key[i]];
+            }
+        }
+
+        private void SubtractByModule()
+        {
+            subtractByModule = new byte[encodedMessageNumbered.Length];
+
+            for (int i = 0; i < encodedMessageNumbered.Length; i++)
+            {
+                subtractByModule[i] = (byte)(encodedMessageNumbered[i] + parameters.Mod - keyMaskNumbered[i]);
+            }
+        }
+        private void SumByModule()
+        {
+            sumByModule = new byte[messageNumbered.Length];
+
+            for (int i = 0; i < messageNumbered.Length; i++)
+            {
+                sumByModule[i] = (byte)((byte)(messageNumbered[i] + keyMaskNumbered[i]) % parameters.Mod);
+            }
+        }
+
+        private void Encrypt()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < sumByModule.Length; i++)
+            {
+                sb.Append(alphabet.Alphabet.FirstOrDefault(d => d.Value == sumByModule[i]).Key);
+            }
+
+            encodedMessage = sb.ToString();
+        }
+        private void Decrypt()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < subtractByModule.Length; i++)
+            {
+                sb.Append(alphabet.Alphabet.FirstOrDefault(d => d.Value == subtractByModule[i]).Key);
+            }
+
+            decodedMessage = sb.ToString();
         }
     }
 }
