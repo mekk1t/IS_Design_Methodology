3af34bba36e0c1e602cb40b694cc175cbcbf8398
mekk1t <kit.kalimov@gmail.com>
2020-10-18 14:23:35 +0300

    Разделение главного окна на Шифрование и Дешифрование

---
+++ Mekk1tCryptography/CustomCodec/MVVM/Views/Decoding.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.ComponentModel;
+
+namespace CustomCodec_WPF.MVVM.Views
+{
+    public partial class DecodingWindow : Window
+    {
+        public DecodingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReturnToSelection(object sender, CancelEventArgs e)
+        {
+            new OperationSelection().Show();
+        }
+    }
+}
---
+++ Mekk1tCryptography/CustomCodec/MVVM/Views/Encoding.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CustomCodec_WPF.MVVM.Views;
+using System.Windows;
+using System.ComponentModel;
+
+namespace CustomCodec
+{
+    public partial class EncodingWindow : Window
+    {
+        public EncodingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            new OperationSelection().Show();
+        }
+    }
+}
