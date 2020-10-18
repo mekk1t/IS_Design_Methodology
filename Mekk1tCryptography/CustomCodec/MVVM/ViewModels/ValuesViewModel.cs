91f4d7b095f456e81b493a848be83a174f751d30
mekk1t <kit.kalimov@gmail.com>
2020-10-18 14:59:39 +0300

    Наброски рабочих Binding-элементов в окне "Данные для шифрования"

---
+++ Mekk1tCryptography/CustomCodec/MVVM/ViewModels/ValuesViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+
+namespace CustomCodec_WPF.MVVM.ViewModels
+{
+    public class ValuesViewModel : INotifyPropertyChanged
+    {
+        private byte _mod;
+        public byte Mod
+        {
+            get
+            {
+                return _mod;
+            }
+            set
+            {
+                _mod = value;
+                NotifyPropertyChanged(nameof(Mod));
+                NotifyPropertyChanged(nameof(AreValuesTypedIn));
+            }
+        }
+
+        private string _key;
+        public string Key
+        {
+            get
+            {
+                return _key;
+            }
+            set
+            {
+                _key = value;
+                NotifyPropertyChanged(nameof(Key));
+                NotifyPropertyChanged(nameof(AreValuesTypedIn));
+            }
+        }
+
+        public bool AreValuesTypedIn
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(Key) && Mod != default;
+            }
+        }
+
+        public ValuesViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
