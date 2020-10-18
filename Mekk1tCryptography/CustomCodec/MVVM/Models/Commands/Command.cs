05271fb181ef048bab7e3ab007ac89fe8253acbc
mekk1t <kit.kalimov@gmail.com>
2020-10-18 22:18:10 +0300

    Реализация команды для выполнения операций в паттерне MVVM

---
+++ Mekk1tCryptography/CustomCodec/MVVM/Models/Commands/Command.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CustomCodec_WPF.MVVM.Models.Commands
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action action;
+        private Func<object> function;
+        private Action<object> _action;
+        private bool canExecute;
+
+        public Command(Action action, bool canExecute = true)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public Command(Func<object> function, bool canExecute = true)
+        {
+            this.function = function;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
