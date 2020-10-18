a100ff67062ae09bf41b4b1acb1a7ecb45f4eef9
mekk1t <kit.kalimov@gmail.com>
2020-10-18 13:59:23 +0300

    Окно выбора операции

---
+++ Mekk1tCryptography/CustomCodec/MVVM/Views/OperationSelection.xaml.cs
@@ -0,0 +1,23 @@
+﻿using CustomCodec;
+using System.Windows;
+
+namespace CustomCodec_WPF.MVVM.Views
+{
+    public partial class OperationSelection : Window
+    {
+        public OperationSelection()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenDecoding(object sender, RoutedEventArgs e)
+        {
+            new DecodingWindow().Show();
+        }
+
+        private void OpenEncoding(object sender, RoutedEventArgs e)
+        {
+            new EncodingWindow().Show();
+        }
+    }
+}
