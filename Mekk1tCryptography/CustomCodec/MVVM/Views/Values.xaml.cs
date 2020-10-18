d46bb8ed02dfca9de9e6ee6095eabf35dd37c8e1
mekk1t <kit.kalimov@gmail.com>
2020-10-18 14:22:57 +0300

    NEW: Окно данных для работы алгоритма

---
+++ Mekk1tCryptography/CustomCodec/MVVM/Views/Values.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CustomCodec_WPF.MVVM.Views
+{
+    public partial class Values : Window
+    {
+        public Values()
+        {
+            InitializeComponent();
+        }
+    }
+}
