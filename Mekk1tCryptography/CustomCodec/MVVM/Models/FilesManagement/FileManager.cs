072c3d9e72af95ba6dded873b1c7f5d2aeffb0de
mekk1t <kit.kalimov@gmail.com>
2020-10-25 12:28:17 +0300

    Реализовано чтение текстовых файлов

--- Mekk1tCryptography/CustomCodec/MVVM/Models/FilesManagement/FileManager.cs
+++ Mekk1tCryptography/CustomCodec/MVVM/Models/FilesManagement/FileManager.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.IO;
 
 namespace CustomCodec.Operations.FilesManagement
@@ -8,6 +9,10 @@ namespace CustomCodec.Operations.FilesManagement
         public string ReadFromFile()
         {
             var ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = Environment.CurrentDirectory;
+            ofd.Filter = "Текстовые файлы (*.txt)|*.txt";
+
             if (ofd.ShowDialog() == true)
             {
                 return File.ReadAllText(ofd.FileName);
